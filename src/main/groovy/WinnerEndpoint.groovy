6201de9b498594728510888c30d1a003af11d9c7
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2019-02-18 20:44:04 +0200

    Testing what makes Winners work again

--- src/main/groovy/WinnerEndpoint.groovy
+++ src/main/groovy/WinnerEndpoint.groovy
@@ -22,7 +22,7 @@ class WinnerEndpoint extends GroovyChainAction {
               flatMap { input ->
                 gameService.insert(
                         input.get("nickname").asText(),
-                        input.get("prizetier")
+                        input.get("prizetier").asInt()
                 )
               }.
               single().
